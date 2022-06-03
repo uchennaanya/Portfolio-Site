901ef7d8760da5fa35df43a2b64e6cbab6224413
uchenna <uchennaongit@gmail.com>
2022-06-03 18:46:36 +0100

    scroll-effect

--- assets/js/js.js
+++ assets/js/js.js
@@ -18,4 +18,33 @@ function scrollFunction() {
   }
 }
 
+let toggle = document.getElementById("toggle")
+let show = document.getElementById("show")
 
+toggle.addEventListener("click", () => {
+  if(show.classList.contains("hide")) {
+      show.classList.remove("hide")
+      show.classList.add("show")
+      // console.log("Hey I was clicked!")
+
+  } else {
+      show.classList.add("hide")
+      show.classList.remove("show")
+      console.log("Hey I was clicked!")
+
+  }
+  // console.log("Hey I was clicked!")
+
+})
+
+var ul = document.getElementById("ul");
+var links = ul.getElementsByClassName("link");
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) {
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
